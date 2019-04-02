e82b2c5b0b7a151f443e4fb3d11e3e9edef49798
LARMUSEAU <adriaan.larmuseau@gmail.com>
2019-04-02 11:05:47 +0800

    switched to static -fPIC for ndk19

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Forked from git://github.com/Rupan/gmp.git
 
 Removed non-usefull compilation targets, built for API level 23 with ndk 19
 
+Currently built statically, with -fPIC, no assembly
+
 set ANDROID_NDK_ROOT before building
 
 
--- x86/gmp.h
+++ x86/gmp.h
@@ -2317,7 +2317,7 @@ enum
 
 /* Define CC and CFLAGS which were used to build this version of GMP */
 #define __GMP_CC "i686-linux-android-gcc"
-#define __GMP_CFLAGS "-O2 -g -pedantic -Wa,--noexecstack -fomit-frame-pointer -ffunction-sections -funwind-tables -fstrict-aliasing -funswitch-loops -finline-limit=300 -no-canonical-prefixes -fstack-protector -march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
+#define __GMP_CFLAGS "-O2 -fPIC -pedantic -Wa,--noexecstack -fomit-frame-pointer -ffunction-sections -funwind-tables -fstrict-aliasing -funswitch-loops -finline-limit=300 -no-canonical-prefixes -fstack-protector -march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
 
 /* Major version number is the value of __GNU_MP__ too, above. */
 #define __GNU_MP_VERSION            6
